cf6f36ae6385888a65e70b5a89d7c2311fdbee35
joyakhanolu <joyakhanolu17@gmail.com>
2023-03-21 01:16:53 +0100

    Mood Mentor

--- models/advice.js
+++ models/advice.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
 const adviceSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
+  title: {
+    type: String, 
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['health', 'relationship', 'career', 'lifestyle', 'religion'],
+    required: true
+  },
 });
 
 const Advice = mongoose.model('Advice', adviceSchema);
--- routes/AdviceRouter.js
+++ routes/AdviceRouter.js
@@ -3,12 +3,8 @@ const AdviceController = require('../controllers/AdviceController');
 
 const router = express.Router();
 
-router.post('/api/advice', (req, res) => {
-    AdviceController.adviceUser
-});
+router.post('/api/advice', AdviceController.adviceUser);
 
-router.get('/api/advice', (req, res) => {
-    AdviceController.getAllAdvice
-});
+router.get('/api/advice', AdviceController.getAllAdvice);
 
 module.exports = router;
